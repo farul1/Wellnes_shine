749c6a45440a9767f0e388789a582efde8c73d3f
angelicayy <164759090+angelicayy@users.noreply.github.com>
2024-06-11 10:41:58 +0700

    Add files via upload

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,39 @@
+    <div class="row mt-4 align-items-center">
+        <div class="col-md-6">
+            <div class="content-text">
+                Wellness & Shine adalah destinasi utama untuk produk kecantikan dan kosmetik yang menjanjikan kilau dan kesehatan bagi pelanggan. Berbasis di Surabaya, perusahaan ini menawarkan berbagai pilihan produk dari merek-merek terkemuka yang telah teruji kualitasnya. Dari perawatan kulit hingga riasan, Wellness & Shine menawarkan solusi lengkap untuk memenuhi kebutuhan kecantikan setiap individu. Dengan website yang user-friendly, pelanggan dapat dengan mudah menelusuri dan membeli produk favorit mereka, sambil mendapatkan tips kecantikan yang berguna. Keunggulan perusahaan ini tidak hanya terletak pada produk yang mereka tawarkan, tetapi juga pada komitmen mereka untuk mempromosikan kecantikan yang sehat dan berkelanjutan.
+            </div>
+        </div>
+        <div class="col-md-6">
+            <img src="{{ asset('assets/images/logo3.png') }}" style="width:60%" alt="">
+        </div>
+    </div>
+
+        <div class="col-md-7">
+            <div class="card">
+                <div class="card-header text-center text-white">
+                    <h2><i class="fas fa-comments"></i> Beri Kritik dan Saran</h2>
+                </div>
+
+                <div class="card-body">
+                    <p class="p-0 mb-5 text-lg-center">Masukan kritik dan saran anda kepada aplikasi kami ini agar kami dapat memberikan
+                        apa yang menjadi kebutuhan anda dan kami dapat berkembang lebih baik lagi.
+                    </p>
+                    <div class="mb-3 row">
+                        <label for="email" class="col-sm-2 col-form-label">Email</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="email" value=""
+                                placeholder="Masukan email Anda">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="pesan" class="col-sm-2 col-form-label">Pesan</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="pesan" placeholder="Masukan Pesan Anda">
+                        </div>
+                    </div>
+                    <button class="btn btn-primary mt-4 w-100"> Kirim pesan anda</button>
+                </div>
+            </div>
+        </div>
+    </div>
