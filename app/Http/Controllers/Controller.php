44e0887d2c24d03280b8f30abcab2d7552ef222d
farulSP <syafarul.priwantoro@gmail.com>
2024-06-11 09:52:59 +0700

    CONTROLLER

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,37 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function shop(Request $request)
+    {
+        if ($request->has('kategory') && $request->has('type')) {
+            $category = $request->input('kategory');
+            $type = $request->input('type');
+            $data = product::where('kategory', $category)
+            ->orWhere('type', $type)->paginate(5);
+        } else {
+            $data = product::paginate(5);
+        }
+        $countKeranjang = tblCart::where(['idUser' => 'guest123', 'status' => 0])->count();
+
+
+        return view('pelanggan.page.shop', [
+            'title'     => 'Shop',
+            'data'      => $data,
+            'count'     => $countKeranjang,
+        ]);
+    }
+
+    public function transaksi()
+    {
+        $db = tblCart::with('product')->where(['idUser' => 'guest123', 'status' => 0])->get();
+        $countKeranjang = tblCart::where(['idUser' => 'guest123', 'status' => 0])->count();
+
+        // dd($db->product->nama_product);die;
+        return view('pelanggan.page.transaksi', [
+            'title'     => 'Transaksi',
+            'count'     => $countKeranjang,
+            'data'      => $db
+        ]);
+    }
 }
