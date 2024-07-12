cb7d3ddbd375fbe9f33e62df4a25026bb265e3dc
farulSP <72846589+farul1@users.noreply.github.com>
2024-07-12 19:55:49 +0700

    Update detailTransaksi.blade.php

--- resources/views/pelanggan/page/detailTransaksi.blade.php
+++ resources/views/pelanggan/page/detailTransaksi.blade.php
@@ -19,28 +19,22 @@
     </div>
 
     <script type="text/javascript">
-        // For example trigger on button clicked, or any time you need
         var payButton = document.getElementById('pay-button');
         payButton.addEventListener('click', function() {
-            // Trigger snap popup. @TODO: Replace TRANSACTION_TOKEN_HERE with your transaction token
             window.snap.pay('{{$token}}', {
                 onSuccess: function(result) {
-                    /* You may add your own implementation here */
                     alert("payment success!");
                     console.log(result);
                 },
                 onPending: function(result) {
-                    /* You may add your own implementation here */
                     alert("wating your payment!");
                     console.log(result);
                 },
                 onError: function(result) {
-                    /* You may add your own implementation here */
                     alert("payment failed!");
                     console.log(result);
                 },
                 onClose: function() {
-                    /* You may add your own implementation here */
                     alert('you closed the popup without finishing the payment');
                 }
             })
